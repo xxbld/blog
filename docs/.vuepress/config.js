3d31bee60b1df3bc5572f1e32b1cdb67d2abd52a
xxbld <1024920618@qq.com>
2020-03-10 21:34:59 +0800

    自动生成菜单导航，

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,57 +1,48 @@
-const sidebar = {
-    '/web/': [
-        {
-            title: 'Vue',
-            children: [
-                'vue/vuepress-blog',
-            ]
-        },
-        {
-            title: 'Node',
-            children: [
-                'node/node-environment',
-            ]
-        },
-    ]
-
-}
-
-const nav = [
-    {
-        text: '前端',
-        items: [{
-            text: 'Vue',
-            link: '/web/' + sidebar['/web/'][0]['children'][0]
-        }]
-    },
-    {
-        text: 'GIS',
-        items: [{
-            text: 'Vue',
-            // link: '/web/' + sidebar['/web/'][0]['children'][0]
-        }]
-    }];
+const getConfig = require("vuepress-bar");
+const { nav, sidebar } = themeConfig = getConfig(`${__dirname}/..`, {
+    pinyinNav: true,
+    // addReadMeToFirstGroup: false,
+})
+// docs/readme 不放在sidebar里面
+delete sidebar['/']
+Object.keys(sidebar).forEach(key => {
+    // addReadMeToFirstGroup: false, 无效 移除第一个
+    sidebar[key][0] === '' ? sidebar[key].shift() : 0
+})
 const config = {
-    base: '/bolg',
-    title: '小小冰绿豆的博客',
+    base: '/blog/',
+    title: '小小冰绿豆',
     description: 'hello',
-    head: [
-        ['link', {
-            rel: 'icon',
-            href: '/favicon.ico'
-        }]
-    ],
+    // theme: 'reco',
     themeConfig: {
         repo: 'xxbld/blog',
-        repoLabel: '代码',
+        repoLabel: 'Github',
         editLinks: true,
         editLinkText: '查看原文|编辑此页',
+        lastUpdated: 'Last Updated',
         nav,
         sidebar,
+        // sidebarDepth: 3,
     },
     markdown: {
         // 显示代码行号
         lineNumbers: true
-    }
+    },
+    plugins: [require('./plugins/rpurl')],
+    // extraWatchFiles: [
+    //     '.vuepress/plugins/**/*.js',
+    // ],
+    // chainWebpack: (config, isServer) => {
+    //     const inlineLimit = 10000
+    //     config.module.rule('images')
+    //         .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+    //         .use('url-loader')
+    //         .loader('url-loader')
+    //         .options({
+    //             limit: inlineLimit,
+    //             name: `assets/img/[name].[hash:8].[ext]`
+    //         })
+    // }
 }
+
 module.exports = config;
